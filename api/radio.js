159534de5db5632b91bb3bad47c1c04ed021b23e
Mushak Shareef <sivcanopi@gmail.com>
2025-08-10 11:30:57 +0530

    api

---
+++ api/radio.js
@@ -0,0 +1,25 @@
+// api/radio.js
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const response = await fetch("https://omshanti.in/amudhamazhai");
+    if (!response.ok) {
+      throw new Error(`Stream request failed with status ${response.status}`);
+    }
+
+    res.setHeader("Content-Type", "audio/mpeg");
+
+    // Stream directly to the response
+    response.body.pipe(res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching radio stream");
+  }
+}
