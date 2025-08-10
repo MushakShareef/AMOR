2d5a75088949a92dd156acc69a13bdbe6633f6c4
Mushak Shareef <sivcanopi@gmail.com>
2025-08-10 11:39:43 +0530

    cng2

--- api/radio.js
+++ api/radio.js
@@ -1,7 +1,5 @@
-
-
 export const config = {
-  runtime: 'nodejs18.x',
+  runtime: 'nodejs', // force Node.js instead of Edge
 };
 
 export default async function handler(req, res) {
@@ -22,6 +20,7 @@ export default async function handler(req, res) {
 
     res.setHeader("Content-Type", "audio/mpeg");
 
+    // Pipe the audio stream directly to the client
     response.body.on('error', err => {
       console.error('Stream error:', err);
       res.end();
